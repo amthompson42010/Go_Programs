a9db84994f32f201e1586a7eeee529d9308c26ea
Alexander Mark Thompson <amthompson42010@gmail.com>
2017-02-09 21:45:52 -0600

    Learning Go programming Language

---
+++ values.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//Addition for Strings
+	fmt.Println("go" + "lang")
+	
+	//Addition for integers
+	fmt.Println("1+1 =", 1+1)
+
+	//Division for float numbers
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	
+	//True and false = false 
+	fmt.Println(true && false)
+	
+	//True or false = true
+	fmt.Println(true || false)
+
+	//Not true = false
+	fmt.Println(!true)
+}
+
